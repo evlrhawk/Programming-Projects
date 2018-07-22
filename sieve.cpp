a1e8c0e062b3106af61c3ceb9f205793e06abc74
Thomas Burr <tb4cmcle@gmail.com>
2018-07-22 00:18:18 -0600

    Sieve 1
    Beggining of Sieve project

---
+++ sieve.cpp
@@ -0,0 +1,29 @@
+/**************************************
+* Author: Thomas Burr 
+* Sieve of Eratosthenes
+* Concept: Find all the prime numbers 
+* less than the given number, n.
+**************************************/
+#include <iostream>
+
+using namespace std;
+
+// Prompts for number, n 
+int getNumber()
+{
+    int number = 0;
+
+    cout << "Input a whole number to find prime number count: ";
+    cin >> number; 
+    cout << endl;
+
+    return number;
+}
+
+// Driver Method
+int main()
+{
+    int number = getNumber();
+
+    vector<int>;
+}
